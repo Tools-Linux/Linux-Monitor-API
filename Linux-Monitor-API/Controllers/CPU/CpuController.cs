f8a4ff989cc11be3da9be1d666fa27752175b9b0
RomzyyTV <140884344+RomzyyTV@users.noreply.github.com>
2026-07-18 13:38:15 +0200

    Create CpuController.cs

---
+++ Linux-Monitor-API/Controllers/CPU/CpuController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Linux_Monitor_API.Controllers.CPU;
+
+[ApiController]
+[Route("api/cpu")]
+public class CpuController  : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var first = ReadCpu();
+        await Task.Delay(500);
+        var second = ReadCpu();
+
+        var idle = second.Idle - first.Idle;
+        var total = second.Total - first.Total;
+
+        var usage = (1.0 - (double)idle / total) * 100;
+
+        return Ok(new
+        {
+            usage = Math.Round(usage, 2)
+        });
+    }
+    
+    private static (long Idle, long Total) ReadCpu()
+    {
+        var line = System.IO.File.ReadLines("/proc/stat").First();
+
+        var values = line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Skip(1)
+            .Select(long.Parse)
+            .ToArray();
+
+        long idle = values[3] + values[4];
+        long total = values.Sum();
+
+        return (idle, total);
+    }
+}
