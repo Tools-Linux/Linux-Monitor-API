0888cc535ed25b62f186938ec4c039338171bc6d
RomzyyTV <140884344+RomzyyTV@users.noreply.github.com>
2026-07-18 13:41:14 +0200

    add disk controller

---
+++ Linux-Monitor-API/Controllers/Disk/DiskController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Linux_Monitor_API.Controllers.Disk;
+
+[ApiController]
+[Route("api/disk")]
+public class DiskController : ControllerBase
+{
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var drive = new DriveInfo("/");
+
+        if (!drive.IsReady)
+        {
+            return StatusCode(500, "Le disque n'est pas disponible");
+        }
+
+        long total = drive.TotalSize;
+        long free = drive.AvailableFreeSpace;
+        long used = total - free;
+        
+        return Ok(new
+        {
+            totalGb = Math.Round(total / 1024d / 1024 / 1024, 2),
+            usedGb = Math.Round(used / 1024d / 1024 / 1024, 2),
+            freeGb = Math.Round(free / 1024d / 1024 / 1024, 2),
+            usage = Math.Round((double)used / total * 100, 2)
+        });
+    }
+}
