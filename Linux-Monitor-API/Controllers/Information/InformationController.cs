e3091fa698cd9d9b6e43307d3d764c0e0bdeb28e
RomzyyTV <140884344+RomzyyTV@users.noreply.github.com>
2026-07-19 13:40:59 +0200

    change uptime seconde to D/H/M/S

--- Linux-Monitor-API/Controllers/Information/InformationController.cs
+++ Linux-Monitor-API/Controllers/Information/InformationController.cs
@@ -11,14 +11,20 @@ public class InformationController : ControllerBase
     public async Task<IActionResult> Get()
     {
         var content = await System.IO.File.ReadAllTextAsync("/proc/uptime");
-        var uptimesecond = double.Parse(
-            content.Split(' ')[0], CultureInfo.InvariantCulture);
+        var uptimeSeconds = double.Parse(
+            content.Split(' ')[0],
+            CultureInfo.InvariantCulture);
 
-        var uptime = TimeSpan.FromSeconds(uptimesecond);
+        var uptime = TimeSpan.FromSeconds(uptimeSeconds);
+
+        var formattedUptime = $"{uptime.Days} jour{(uptime.Days > 1 ? "s" : "")}, " +
+                              $"{uptime.Hours} heure{(uptime.Hours > 1 ? "s" : "")}, " +
+                              $"{uptime.Minutes} minute{(uptime.Minutes > 1 ? "s" : "")}, " +
+                              $"{uptime.Seconds} seconde{(uptime.Seconds > 1 ? "s" : "")}";
 
         return Ok(new
         {
-            time = uptime
+            time = formattedUptime
         });
     }
 }
