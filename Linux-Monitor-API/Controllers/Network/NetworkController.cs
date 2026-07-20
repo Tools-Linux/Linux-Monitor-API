78c6c8f458d9a73e703d8d5e008c98c3f5d2c5ff
RomzyyTV <140884344+RomzyyTV@users.noreply.github.com>
2026-07-20 13:24:52 +0200

    add networkcontroller

---
+++ Linux-Monitor-API/Controllers/Network/NetworkController.cs
@@ -0,0 +1,86 @@
+﻿using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Linux_Monitor_API.Controllers.Network;
+
+[ApiController]
+[Route("api/network")]
+public class NetworkController : ControllerBase
+{
+    private static readonly Dictionary<string, (ulong Rx, ulong Tx)> Previous = new();
+    
+    [HttpGet]
+    public IActionResult GetNetwork()
+    {
+        var interfaces = new List<object>();
+
+        foreach (var nic in NetworkInterface.GetAllNetworkInterfaces())
+        {
+            try
+            {
+                var name = nic.Name;
+
+                var rxPath = $"/sys/class/net/{name}/statistics/rx_bytes";
+                var txPath = $"/sys/class/net/{name}/statistics/tx_bytes";
+
+                if (!System.IO.File.Exists(rxPath) ||
+                    !System.IO.File.Exists(txPath)) 
+                    continue;
+
+
+                ulong rxBytes = ulong.Parse(System.IO.File.ReadAllText(rxPath));
+
+                ulong txBytes = ulong.Parse(System.IO.File.ReadAllText(txPath));
+
+
+                double rxMbps = 0;
+                double txMbps = 0;
+
+
+                if (Previous.TryGetValue(name, out var old))
+                {
+                    rxMbps = ((rxBytes - old.Rx) * 8d) / 1_000_000d;
+                    txMbps = ((txBytes - old.Tx) * 8d) / 1_000_000d;
+                }
+                
+                Previous[name] = (
+                    rxBytes,
+                    txBytes
+                );
+
+                var ip = nic.GetIPProperties()
+                    .UnicastAddresses
+                    .FirstOrDefault(x =>
+                        x.Address.AddressFamily == AddressFamily.InterNetwork)
+                    ?.Address
+                    .ToString() ?? "";
+
+
+                var mac = string.Join(":",
+                    nic.GetPhysicalAddress()
+                        .GetAddressBytes()
+                        .Select(x => x.ToString("X2"))
+                );
+                
+                interfaces.Add(new
+                {
+                    name,
+                    ip,
+                    mac,
+                    status = nic.OperationalStatus == OperationalStatus.Up? "up" : "down",
+                    speedMbps = nic.Speed > 0 ? nic.Speed / 1_000_000 : 0,
+
+                    rxMbps = Math.Round(rxMbps, 2),
+                    txMbps = Math.Round(txMbps, 2),
+                    rxBytes,
+                    txBytes
+                });
+            }
+            catch
+            {
+            }
+        }
+        return Ok(interfaces);
+    }
+}
