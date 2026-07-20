0601b01449dd338e74985ad038d1ecaeaa66bf48
RomzyyTV <140884344+RomzyyTV@users.noreply.github.com>
2026-07-20 20:30:04 +0200

    debug???

--- Linux-Monitor-API/Models/Disk/LsblkRoot.cs
+++ Linux-Monitor-API/Models/Disk/LsblkRoot.cs
@@ -1,21 +1,32 @@
-﻿public class LsblkDevice
+﻿using System.Text.Json.Serialization;
+
+public class LsblkDevice
 {
+    [JsonPropertyName("name")]
     public string Name { get; set; } = "";
+
+    [JsonPropertyName("size")]
     public long Size { get; set; }
 
+    [JsonPropertyName("model")]
     public string? Model { get; set; }
 
+    [JsonPropertyName("type")]
     public string Type { get; set; } = "";
 
+    [JsonPropertyName("mountpoint")]
     public string? MountPoint { get; set; }
 
+    [JsonPropertyName("fstype")]
     public string? FsType { get; set; }
 
+    [JsonPropertyName("children")]
     public List<LsblkDevice>? Children { get; set; }
 }
 
 
 public class LsblkRoot
 {
+    [JsonPropertyName("blockdevices")]
     public List<LsblkDevice> Blockdevices { get; set; } = [];
 }
